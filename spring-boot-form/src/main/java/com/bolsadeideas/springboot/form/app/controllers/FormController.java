9575c28a79175303eae76e7b40009448093f1794
jossrc <kuro.rc10@gmail.com>
2021-07-24 17:28:44 -0500

    Método handler para recibir y procesar los datos enviados

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -4,17 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model) {
+        model.addAttribute("titulo", "Formulario usuarios");
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(Model model) {
+    public String procesar(Model model,
+           @RequestParam(name="txtUsername") String username,
+           @RequestParam(name="txtEmail") String email,
+           @RequestParam(name="txtPassword") String password) {
+
+        model.addAttribute("titulo", "Resultado form");
+        model.addAttribute("username", username);
+        model.addAttribute("password", email);
+        model.addAttribute("email", password);
+
         return "resultado";
     }
 
