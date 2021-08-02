a42110eba729e48b949c0af4d205043bfdd7b891
jossrc <kuro.rc10@gmail.com>
2021-08-02 14:21:34 -0500

    NumberFormatException

--- 06-spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
+++ 06-spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/AppController.java
@@ -8,7 +8,8 @@ public class AppController {
 
     @GetMapping("index")
     public String index() {
-        Integer valor = 100/0;
+        //Integer valor = 100/0;
+        Integer valor = Integer.parseInt("10x");
         return "index";
     }
 
