a6204eaff0340feeef3ffe9db354c650a2fd54b5
jossrc <kuro.rc10@gmail.com>
2021-08-04 17:47:09 -0500

    Creando la vista listar.html

---
+++ 07-spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'Andres', 'Guzman', 'andgusman@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'Jose', 'Robles', 'joserobles@gmail.com', '2018-05-11');
