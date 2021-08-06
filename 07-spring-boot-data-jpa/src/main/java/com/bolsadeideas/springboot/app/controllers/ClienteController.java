a9ae525adc13b49e2db9bffd0db0c7a89b159b38
jossrc <kuro.rc10@gmail.com>
2021-08-06 14:31:24 -0500

    Agregando crear en Repositorio DAO y Controlador

--- 07-spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ 07-spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -1,11 +1,15 @@
 package com.bolsadeideas.springboot.app.controllers;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Map;
 
 @Controller
 public class ClienteController {
@@ -19,6 +23,20 @@ public class ClienteController {
         model.addAttribute("titulo", "Listado de clientes");
         model.addAttribute("clientes", clienteDAO.findAll());
         return "listar";
+    }
+
+    @GetMapping("/form")
+    public String crear(Map<String, Object> model){
+        Cliente cliente = new Cliente();
+        model.put("cliente", cliente);
+        model.put("titulo", "Formulario de Cliente");
+        return "form";
+    }
+
+    @PostMapping(value = "/form")
+    public String guardar(Cliente cliente) {
+        clienteDAO.save(cliente);
+        return "redirect:listar";
     }
 
 }
--- 07-spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
+++ 07-spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
@@ -22,4 +22,10 @@ public class ClienteDaoImpl implements IClienteDao {
     public List<Cliente> findAll() {
         return entityManager.createQuery("from Cliente").getResultList();
     }
+
+    @Override
+    @Transactional
+    public void save(Cliente cliente) {
+        entityManager.persist(cliente);
+    }
 }
